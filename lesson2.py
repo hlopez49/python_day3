a11dd5817600ecabea316c3c0e8a57f3823fcd80
Lopez <HLopez49@cps.k12.il.us>
2024-08-30 15:06:31 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,63 @@
+#create a print a function that prints out hellow world
+# print("Hello, World!")
+# day_of_the_week= input("What is the day of the week?")
+# print("The day of the week is" + day_of_the_week)
+
+# concatenation is when you add two strings together
+# using a plus sign
+
+# movies_this_week = input("What movies are you watching this week? ")
+# print("I am watch" + movies_this_week + "this week")
+
+# mood= input("How are you feeling today? ")
+# print("I am feeling " + mood ")
+
+# data type variables in python 
+
+#strings - text
+name= "john"
+#whenever it is wrapped in quotes it is a string 
+#even its a number 
+
+# integers - whole numbers
+# year = 2024
+# # does not need quotes around it
+# fouryearslater= 2028
+# subtract = fouryearslater - year
+# print(subtract)
+ 
+# # floats - decimal numbers
+# priceBigMac = 3.99
+# priceDoublePounder = 4.99
+# totalPrice= priceBigMac + priceDoublePounder
+# print(totalPrice)
+
+# booleans - true or false
+# isRaining = False
+# print(isRaining)
+
+# lists- a collection of items
+# groceries = ["apples", "bananas","carrots"]
+# print(groceries)
+
+movies = ["deadpool", "wovlerine", "Dispicable Me"]
+
+address = "5507 South Candyman Ave"
+
+movies_time = "3:30 PM"
+dinner_time = "6:30 PM"
+
+movie_name = "dispicable me 3"
+moviePrice= 8.50
+DinnerPrice= 40.55
+
+isMovieinEvening= False
+
+#orginal 
+# howManyPeopleGoing= 8
+
+#Mr. Evins' version 
+
+howManyPeopleGoing =input("How many people are going? " )
+#type conversion converting from one data type to another
+print(" wee are going to see" + movie_name + "at" movies_time + "and then dinner at " dinner_time + "The address is " + address )
